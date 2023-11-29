0409169c7fc8b94aba482b41b11ca69fe7e9495b
Nathan Rydel <nathanrydel@gmail.com>
2023-11-29 13:37:58 -0700

    added scaffolding and TODOs

--- app.py
+++ app.py
@@ -1,8 +1,40 @@
 # Put your app in here.
 from flask import Flask, request
-from operations import add,sub,mult,div
+from operations import add, sub, mult, div
 
 app = Flask(__name__)
 
 
+@app.get("/add")
+def call_add():
+    # TODO: Add docstring
 
+    # TODO: implement function
+    ...
+
+
+@app.get("/sub")
+def call_sub():
+    # TODO: Add docstring
+
+    # TODO: implement function
+
+    ...
+
+
+@app.get("/mult")
+def call_mult():
+    # TODO: Add docstring
+
+    # TODO: implement function
+
+    ...
+
+
+@app.get("/div")
+def call_div():
+    # TODO: Add docstring
+
+    # TODO: implement function
+
+    ...
